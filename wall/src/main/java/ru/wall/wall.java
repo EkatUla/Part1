c5ec3ac554eb84265945fdfea844bb5f3d37e09c
EkatUla2022 <reikon@list.ru>
2023-02-28 20:24:30 +0300

    fix again task01 28022023

--- wall/src/main/java/ru/wall/wall.java
+++ wall/src/main/java/ru/wall/wall.java
@@ -7,24 +7,26 @@ public class wall {
         int letters = 15;
         int whitespace = 3;
         int suggestionLength = ((letters / 3) * 62) + (whitespace * 12);
+        System.out.println(suggestionLength + " длинна предложения");
 
         Scanner sc = new Scanner(System.in);
 
         try {
-            System.out.println("Введите длинну забора в сантиметрах: ");
+            System.out.println("Введите длинну забора - положительное, целое число в сантиметрах без пробелов: ");
             String lengthUser = sc.nextLine();
             int enterInt = Integer.parseInt(lengthUser);
-            if (enterInt <= suggestionLength) {
-                System.out.println("Ваше предложение поместиться на заборе");
+            if (enterInt < suggestionLength) {
+                System.out.println("Жаль на забор длинной " + enterInt + " см не поментиться предложение");
             } else {
-                System.out.println("Ваше предложение слишном длинное и НЕ поместиться на заборе");
+                System.out.println("Здорово на таком заборе предложение поместиться");
             }
             //throw new RuntimeException();
         } catch (NumberFormatException e) {
-            System.out.println("Вы ввели не число или пробел, попробуйте снова ввести целое число ");
+            System.out.println("Вы ввели не число или пробел или число с пробелом или десятичное число, попробуйте снова ввести целое число без пробелов");
         } //catch (Exception e) {
         //System.out.println("Undefined error");
         //}
 
     }
+
 }
